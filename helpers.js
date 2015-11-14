4a6f70975895d7b68d21e33cc60c92a64ff44f83
Karen Ang <thekarenang@gmail.com>
2015-11-14 21:41:15 +0800

    Modularise helper functions

--- helpers.js
+++ helpers.js
@@ -2,8 +2,14 @@ var request = require('request');
 var crypto = require('crypto');
 
 var settings = require('./settings');
+var gph = require('./graphite');
+var ig = require('./instagram');
 
-
+/**
+ * Verify if the request is really from Instagram
+ * @param   request The request POSTed from Instagram
+ * @return          boolean indicating if the request is from Instagram
+ */
 exports.isValidRequest = function isValidRequest(request) {
   var hmac = crypto.createHmac('sha1', settings.CLIENT_SECRET);
   hmac.update(request.rawBody);
@@ -12,100 +18,75 @@ exports.isValidRequest = function isValidRequest(request) {
   return providedSignature != calculatedSignature || !request.body
 }
 
-exports.queuePhotos = function queuePhotos(response) {
-  var num_updates = response.length;
-  var tagName = response.params.tagName;
+/**
+ * Retrieve the last igLimit (preset) photos tagged tagName, then
+ * POST them using Graphite
+ * @param   data The data POSTed from Instagram because of our subscription
+ */
 
-  request.get({url: settings.apiHost + "tags/" + tagName +
-   "/media/recent?client_id=" + settings.CLIENT_ID + "&count=" + num_updates},
-   function(err, response, body) {
-     if (!error && response.statusCode == 200) {
-       urls = getUrlsFrom(response.data);
-       attachment_ids = postToGraphite(urls, 'attachments');
-       postToGraphite(urls, 'orders', attachment_ids);
+exports.queuePhotos = function queuePhotos(data) {
+  var num_updates = settings.igLimit;
+  var tagName = data.params.tagName;
+  var update_url = settings.apiHost + "tags/" + 'graphite' +
+   "/media/recent?client_id=" + settings.CLIENT_ID + "&count=" + num_updates
+  var data;
+  request.get({url: update_url},
+   function(err, res, body) {
+     if (!err && res.statusCode == 200) {
+       instagram = JSON.parse(body);
+       postInstagramData(instagram.data);
      } else {
        console.log("Error retrieving latest photos");
      }
    })
 }
 
+/**
+ * POST the photos we have retrieved using Graphite
+ * @param   data The parsed Instagram data; an array of photos
+ */
+function postInstagramData(data) {
+  var photoUrls = [];
+  var attPromise;
+
+  var headers, content;
+
+  photoUrls = getUrlsFrom(data)
+
+  headers = {
+    'Authorization': 'Bearer ' + settings.GPH.AUTH_TOKEN,
+  }
+
+  for (var i = 0; i < photoUrls.length; i++) {
+    content = {
+      'storage_url': photoUrls[i],
+    }
+    attPromise = new Promise(function(resolve, reject) {
+      gph.postAttachment(headers, content, resolve, reject)
+    });
+    attPromise
+    .then(gph.postOrder)
+    .catch(rejectHandler)
+  }
+}
+
+/**
+ * Extract all the urls from the Instagram data
+ * @param   data Parsed Instagram datal an array of photos
+ * @return       An array of urls which can be POSTed to JX Print
+ */
+
 function getUrlsFrom(data) {
   var results = [];
-  var obj, url;
+  var img, url;
   for (var i = 0; i<data.length; i++) {
-    obj = data[i]
-    url = obj.images.standard_resolution.url
+    img = data[i]
+    url = img.images.standard_resolution.url
     results.push(url);
   }
+  return results;
 }
 
-function postToGraphite(urls, type, att) {
-  var params;
-  if (type == 'attachments') {
-    var ids = [];
-    for (var i = 0; i<urls.length; i++) {
-      params = {
-        storage_url: urls[i]
-      }
-      setTimeout(function() {
-        request.post({url: settings.GPH.POST_URL + 'attachments', form: params},
-          function(err, response, body) {
-            if (!err && response.statusCode == 200)
-              ids.push(response.attachment.id)
-          }, 3000)
-      });
-    }
-    return ids;
-  } else if (type == 'orders') {
-    for (var i = 0; i<urls.length; i++) {
-      params = {
-        customer_id: settings.GPH.CUSTOMER_ID,
-        shipping_address_id: settings.GPH.SHIPPING_ADDRESS_ID,
-        order_items: [{
-          item_id: settings.GPH.ITEM_ID,
-          quantity: 1,
-          size_id: settings.GPH.SIZE_ID,
-          attachment_ids: att[i]
-        }]
-      }
-      setTimeout(function() {
-        request.post({url: settings.GPH.POST_URL + 'orders', form: params},
-          function(err, response, body) {
-            if (!err && response.statusCode == 200)
-              console.log('Posted order to Graphite')
-            else console.log('Error posting order to Graphite')
-        })
-      }, 3000);
-    }
-  }
-}
-
-exports.subscribeToTag = function subscribeToTag(tagName) {
-  var params = {
-    client_id: settings.CLIENT_ID,
-    client_secret: settings.CLIENT_SECRET,
-    verify_token: 'tag-sub',
-    object: "tag",
-    aspect: "media",
-    object_id: tagName,
-    callback_url: "http://" + settings.host + "/callbacks/tag/" + tagName
-  };
-
-  request.post({url: settings.apiHost + "subscriptions", form: params},
-    function(err, response, body) {
-      console.log(response);
-      if (!err && response.statusCode == 200)
-        console.log("Subscribed to tag: ", tagName);
-      else console.log("Failed to subscribe: ", err);
-  });
-
-exports.unsubAll = function unsubAll() {
-  request.del({url: settings.apiHost + "subscriptions?client_secret=" +
-  settings.CLIENT_SECRET + "&client_id=" + settings.CLIENT_ID + "&object=all"},
-    function(err, response, body) {
-      if (!err && response.statusCode == 200)
-        console.log("Unsubscribed to all");
-      else console.log("Failed to unsubscribe: ", err);
-    });
-  }
+function rejectHandler(reason) {
+  console.log(reason)
 }
