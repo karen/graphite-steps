e6da41b78f4285a3c45dadd5c09cd9cc4aa71ac9
Karen Ang <thekarenang@gmail.com>
2015-11-18 14:16:01 +0800

    Add username to order

--- graphite.js
+++ graphite.js
@@ -6,7 +6,7 @@ var logger = require('./logger');
 var winston = logger.winston;
 var settings = require('./settings');
 
-exports.postAttachment = function (headers, content, resolve, reject) {
+exports.postAttachment = function (headers, content, username) {
   var options;
 
   options = {
@@ -19,29 +19,80 @@ exports.postAttachment = function (headers, content, resolve, reject) {
 
   var attId;
 
-  setTimeout(function() {
-    request(options, function (err, res, body) {
-      if (!err && res.statusCode === 201) {
-        attId = body.attachment.id;
-        resolve(attId);
-      } else {
-        var reason = res.statusCode + ' ' + err;
-        reject(reason);
-      }
-    });
-  }, 1000);
+  var attPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      request(options, function (err, res, body) {
+        if (!err && res.statusCode === 201) {
+          attId = body.attachment.id;
+          resolve([username, attId]);
+        } else {
+          var reason = res.statusCode + ' ' + err;
+          reject(reason);
+        }
+      });
+    }, 1000);
+  });
+  return attPromise;
 };
 
-exports.postOrder = function (attId) {
+exports.postCustomer = function(pair) {
+  var username = pair[0];
+  var attId = pair[1];
   var headers, content, options;
 
   headers = {
-    'Authorization': 'Bearer ' + settings.GPH.AUTH_TOKEN,
+    'Authorization': 'Bearer ' + settings.GPH.AUTH_TOKEN
+  }
+
+  content = {
+    name: username,
+    email: username + '@hello.com',
+    contact_number: '999',
+    shipping_addresses: [
+      settings.GPH.SHIPPING_ADDRESS
+    ]
+  }
+
+  options = {
+    headers: headers,
+    url: settings.GPH.POST_URL + 'customers',
+    body: content,
+    json: true,
+    method: 'POST'
+  }
+
+  var customerId, shippingAddressId;
+
+  var custPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      request(options, function (err, res, body) {
+        if (!err && res.statusCode === 201) {
+          customerId = body.id;
+          shippingAddressId = body.shipping_addresses[0].id;
+          resolve([customerId, attId, shippingAddressId]);
+        } else {
+          reject(err);
+        }
+      });
+    }, 1000);
+  });
+
+  return custPromise;
+}
+
+exports.postOrder = function (triplet) {
+  var customerId = triplet[0];
+  var attId = triplet[1];
+  var shippingAddressId = triplet[2];
+  var headers, content, options;
+
+  headers = {
+    'Authorization': 'Bearer ' + settings.GPH.AUTH_TOKEN
   };
 
   content = {
-    'customer_id': settings.GPH.CUSTOMER_ID,
-    'shipping_address_id': settings.GPH.SHIPPING_ADDRESS_ID,
+    'customer_id': customerId,
+    'shipping_address_id': shippingAddressId,
     'order_items': [{
       'combination_id': settings.GPH.COMBINATION_ID,
       'attachment_ids': attId }]
@@ -55,19 +106,22 @@ exports.postOrder = function (attId) {
     method: 'POST'
   };
 
-  setTimeout(function() {
-    request(options, postOrderCallback);
-  }, 1000);
+  var orderUrl;
 
+  var orderPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      request(options, function(err, res, body) {
+        if (!err && res.statusCode === 201) {
+          orderUrl =  settings.GPH.DASHBOARD + body.order_id;
+          resolve(body);
+          winston.info('Posted order to Graphite: ', orderUrl);
+        } else {
+          reject(err);
+          winston.error(code + ': Error posting order to Graphite - ' + err);
+        }
+      });
+    }, 1000);
+  });
+
+  return orderPromise;
 };
-
-function postOrderCallback(err, res, body) {
-  var code = res.statusCode;
-
-  if (!err && res.statusCode === 201){
-    winston.info('Posted order to Graphite', code, body);
-  }
-  else {
-    winston.error(code + ': Error posting order to Graphite - ' + err);
-  }
-}
